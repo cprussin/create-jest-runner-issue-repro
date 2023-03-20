47dd25dd0e923bc0615665508ab6d02bb43b080b
Connor Prussin <connor@prussin.net>
2023-03-19 21:13:44 -0700

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+module.exports = async () => ({
+    projects: [
+        await createJestConfig({
+            displayName: "unit-test"
+        })(),
+        {
+            displayName: "lint",
+            runner: "jest-runner-eslint",
+            testMatch: ["**/*"]
+        }
+    ]
+});
